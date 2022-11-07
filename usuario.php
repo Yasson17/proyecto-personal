d0c2ece00d93862f2236ab74dd939717ffa0a368
yasson17 <saipalomo17@gmail.com>
2022-11-07 11:36:40 -0400

    pequeño cambio

--- usuario.php
+++ usuario.php
@@ -13,7 +13,7 @@ if(isset($_SESSION['usuarios']))
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
   
-  <title>ABAE</title>
+  <title>ABAE </title>
 
 
 <!-- Normalize V8.0.1 -->
